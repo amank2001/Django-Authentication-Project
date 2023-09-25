3c5e876c63193d5bcdb890a70e4e155cb52295fd
amankhan <aman@littlelemon.com>
2023-09-25 17:38:27 +0530

    added edit_profile_complete page

--- authentication/AuthApp/views.py
+++ authentication/AuthApp/views.py
@@ -12,12 +12,12 @@ def dashboard(request):
 @login_required
 def edit_profile(request):
     if request.method == 'POST':
-        form = UserProfileForm(request.POST)
+        form = UserProfileForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return render(request, 'registration/dashboard.html', {'form': form})  
+            return render(request, 'registration/edit_profile_done.html', {'form': form})  
     else:
-        form = UserProfileForm()
+        form = UserProfileForm(instance=request.user)
     return render(request, 'registration/edit_profile.html', {'form': form})
 
 def register(request):
